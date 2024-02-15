d6681eb8307803b43c7d08d6324dd189ea14433e
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-02-15 15:26:29 -0500

    class and object

--- oop/Program.cs
+++ oop/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Coche cohe= new Coche(3,"s");
+
+cohe.GetInfo();
+
+
+class Coche
+{
+  private int codigo;
+  private String marca;
+ //Constructor
+  public Coche(int codigo, String marca)
+  {
+    this.codigo = codigo;
+    this.marca = marca;
+  }
+  //Set and getter
+  public int Codigo { get => codigo; set => codigo = value; }
+  public String Marca { get => marca; set => marca = value; }
+
+  public void  GetInfo(){
+    Console.WriteLine($"Marca {this.Marca} and Codigo {this.Codigo}"); 
+  }
+}
+
+
